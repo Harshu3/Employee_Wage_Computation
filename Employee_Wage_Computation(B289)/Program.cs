e64e15b4b07fce4c9314a5dc952f78b6af01216b
Harshita <chandaliyaharshita222@gmail.com>
2023-06-27 18:33:22 +0530

    Check employee attendance

--- Employee_Wage_Computation(B289)/Program.cs
+++ Employee_Wage_Computation(B289)/Program.cs
@@ -6,7 +6,18 @@ namespace Employee_Wage_Computation
     {
         static void Main(string[] args)
         {
+            int isPresent = 1;
             Console.WriteLine("Welcome to Employee Wage Computation Problem!");
+            Random random = new Random();
+            int empAttendance = random.Next(0, 2); //0 or 1
+            if (empAttendance == isPresent)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
