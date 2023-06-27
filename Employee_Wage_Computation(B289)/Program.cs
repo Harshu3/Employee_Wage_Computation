87246736b3c1788074660c806b2977476c1ad0a8
Harshita <chandaliyaharshita222@gmail.com>
2023-06-27 18:24:16 +0530

    Added Welcome Message

---
+++ Employee_Wage_Computation(B289)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Employee_Wage_Computation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Problem!");
+        }
+    }
+}
