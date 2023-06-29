d83a76d4e0fae7d8271bd8110531760d9b381497
Harshita <chandaliyaharshita222@gmail.com>
2023-06-29 17:19:20 +0530

    Calculate Employee Daily Wage

--- Employee_Wage_Computation(B289)/Program.cs
+++ Employee_Wage_Computation(B289)/Program.cs
@@ -6,18 +6,23 @@ namespace Employee_Wage_Computation
     {
         static void Main(string[] args)
         {
-            int isPresent = 1;
+            int empHrs = 0, isPresent = 1, empWage = 0;
+            const int EMP_RATE_PER_HR = 20;
+
             Console.WriteLine("Welcome to Employee Wage Computation Problem!");
             Random random = new Random();
             int empAttendance = random.Next(0, 2); //0 or 1
             if (empAttendance == isPresent)
             {
                 Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
             }
+            empWage = EMP_RATE_PER_HR * empHrs;
+            Console.WriteLine("Employee Daily Wage is: " + empWage);
         }
     }
 }
