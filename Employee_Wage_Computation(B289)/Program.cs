d5871e4a71e052dfa591840bfd8428052a891a5c
Harshita <chandaliyaharshita222@gmail.com>
2023-06-29 17:26:28 +0530

    Calculate employee Part-time wage

--- Employee_Wage_Computation(B289)/Program.cs
+++ Employee_Wage_Computation(B289)/Program.cs
@@ -7,15 +7,20 @@ namespace Employee_Wage_Computation
         static void Main(string[] args)
         {
             int empHrs = 0, isPresent = 1, empWage = 0;
-            const int EMP_RATE_PER_HR = 20;
+            const int EMP_RATE_PER_HR = 20, IS_FULL_TIME = 1, IS_PART_TIME = 2;
 
             Console.WriteLine("Welcome to Employee Wage Computation Problem!");
             Random random = new Random();
-            int empAttendance = random.Next(0, 2); //0 or 1
-            if (empAttendance == isPresent)
+            int empAttendance = random.Next(0, 3); //0 to 2
+            if (empAttendance == IS_FULL_TIME)
             {
                 Console.WriteLine("Employee is Present");
                 empHrs = 8;
+            }
+            else if (empAttendance == IS_PART_TIME)
+            {
+                Console.WriteLine("Employee is Part-time Present");
+                empHrs = 4;
             }
             else
             {
