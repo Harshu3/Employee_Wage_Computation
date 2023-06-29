cf1752473a11903654beb50cbbdc71a7e153c791
Harshita <chandaliyaharshita222@gmail.com>
2023-06-29 17:41:40 +0530

    Calculating empWage using Switch Case

--- Employee_Wage_Computation(B289)/Program.cs
+++ Employee_Wage_Computation(B289)/Program.cs
@@ -6,25 +6,26 @@ namespace Employee_Wage_Computation
     {
         static void Main(string[] args)
         {
-            int empHrs = 0, isPresent = 1, empWage = 0;
+            int empHrs = 0, empWage = 0;
             const int EMP_RATE_PER_HR = 20, IS_FULL_TIME = 1, IS_PART_TIME = 2;
 
             Console.WriteLine("Welcome to Employee Wage Computation Problem!");
             Random random = new Random();
             int empAttendance = random.Next(0, 3); //0 to 2
-            if (empAttendance == IS_FULL_TIME)
+            switch (empAttendance)
             {
-                Console.WriteLine("Employee is Present");
-                empHrs = 8;
-            }
-            else if (empAttendance == IS_PART_TIME)
-            {
-                Console.WriteLine("Employee is Part-time Present");
-                empHrs = 4;
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
+                case IS_FULL_TIME:
+                    Console.WriteLine("Employee is Full-time Present");
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    Console.WriteLine("Employee is Part-time Present");
+                    empHrs = 4;
+                    break;
+                default:
+                    Console.WriteLine("Employee is Absent");
+                    empHrs = 0;
+                    break;
             }
             empWage = EMP_RATE_PER_HR * empHrs;
             Console.WriteLine("Employee Daily Wage is: " + empWage);
